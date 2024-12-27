63ddc9aca3e40798de1bb23d6673064068d2cbe4
Chet Hill <chetdhill@gmail.com>
2024-12-27 11:33:36 -0600

    Add multiple dynamic components outputting different data (the long wrong way lol)

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImage from './assets/components.png';
+import { CORE_CONCEPTS } from "./data";
+import reactImg from "./assets/react-core-concepts.png";
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 const altText = "Stylized atoms"
 
@@ -40,9 +40,24 @@ function App() {
           <h2>Time to get started!</h2>
           <ul>
             <CoreConcept 
-              title="Components" 
-              description="The core UI building block"
-              image={componentsImage}
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+             />
+             <CoreConcept 
+              title={CORE_CONCEPTS[1].title} 
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+             />
+             <CoreConcept 
+              title={CORE_CONCEPTS[2].title} 
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image}
+             />
+             <CoreConcept 
+              title={CORE_CONCEPTS[3].title} 
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image}
              />
           </ul>
         </section>
