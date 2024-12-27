752ad5579576ae13b0c0a1e208c838f39616557d
Chet Hill <chetdhill@gmail.com>
2024-12-27 13:05:05 -0600

    Children prop: Finish basics

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -28,9 +28,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton props>
-              Components
-            </TabButton>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
           </menu>
         </section>
         
