9be0d452b24966344b4e57a5dbcffdb38db6d769
Chet Hill <chetdhill@gmail.com>
2025-01-01 11:46:33 -0600

    Output content conditionally in React

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,14 +3,15 @@ import { CORE_CONCEPTS } from "./data";
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
 import TabButton from "./components/TabButton/TabButton";
+import { EXAMPLES } from './data.js'
 
 function App() {
   // TODO: ALERT: ONLY CALL HOOKS INSIDE OF COMPONENT FUNCTIONS
   // TODO: ALERT: ONLY CALL HOOKS ON THE TOP LEVEL
-  useState();
+  // useState()
 
   // TODO: useState() always yield exactly two elements. Names are your choice, but the naming convention is [theAction, setTheAction].
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState();
   // first element = current state value: provided by React. May change if the component function is executed again
   // second element = state updating function: updates the stored value & "tells" React to re-execute the component function in which useState() was called
   // useState argument = Initial state value: Stored by React
@@ -48,7 +49,20 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {/* Rendering Content Conditionally: using && could have same results as ternary operator. Syntax slightly different or create a var and output the var in JSX */}
+          {!selectedTopic ? (
+            <p>Please select a topic</p> 
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>
+                  {EXAMPLES[selectedTopic].code}
+                </code>
+              </pre>
+            </div>
+          )}
         </section>
         
       </main>
