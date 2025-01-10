e9a0285f83af1a4b883317f2eba479bf43bad6ce
Chet Hill <chetdhill@gmail.com>
2025-01-10 13:53:22 -0600

    CSS Styling & Dynamic Styling

--- 01-starting-project/src/components/TabButton/TabButton.jsx
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,9 +1,11 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
+    console.log('TABBUTTON COMPONENT EXECUTING');
+    
     return (
         <li>
             {/* Events in React: onClick, etc value should be a function or function call */}
             {/* onClick is an actual event handler, onSelect is a custom name that uses the on-- best practice when something is done i.e. onScroll, onWhatever lol */}
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>{children}</button>
         </li>
     );
 };
